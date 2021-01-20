63ac1affef723fdb30f706b5b6fb681151f71aeb
Shubham <96shubhamshinde@gmail.com>
2021-01-20 11:18:13 +0530

    liveImageStream

--- lib/scan.dart
+++ lib/scan.dart
@@ -1,18 +1,27 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:ffi';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
-
+import 'package:ffi/ffi.dart';
+import 'package:simple_edge_detection/edge_detection.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter_scanner_app/packages/flutter-simple-edge-detection/lib/edge_detection.dart';
+import 'package:simple_edge_detection_example/cropping_preview.dart';
 
+import 'LiveImagePreview.dart';
 import 'camera_view.dart';
 import 'cropping_preview.dart';
 import 'edge_detector.dart';
 import 'image_view.dart';
+import  'package:simple_edge_detection_example/LiveImagePreview.dart';
+import 'package:image/image.dart' as imglib;
+
+typedef convert_func = Pointer<Uint32> Function(Pointer<Uint8>, Pointer<Uint8>, Pointer<Uint8>, Int32, Int32, Int32, Int32);
+typedef Convert = Pointer<Uint32> Function(Pointer<Uint8>, Pointer<Uint8>, Pointer<Uint8>, int, int, int, int);
 
 class Scan extends StatefulWidget {
   @override
@@ -26,12 +35,25 @@ class _ScanState extends State<Scan> {
   String croppedImagePath;
   EdgeDetectionResult edgeDetectionResult;
 
+  bool _cameraInitialized = false;
+  imglib.Image live_img;
+
+  CameraImage _savedImage;
+
+
+  final DynamicLibrary convertImageLib = Platform.isAndroid
+      ? DynamicLibrary.open("libconvertImage.so")
+      : DynamicLibrary.process();
+  Convert conv;
+
   @override
   void initState() {
     super.initState();
     checkForCameras().then((value) {
       _initializeController();
     });
+    // Load the convertImage() function from the library
+    conv = convertImageLib.lookup<NativeFunction<convert_func>>('convertImage').asFunction<Convert>();
   }
 
   @override
@@ -53,9 +75,14 @@ class _ScanState extends State<Scan> {
 
     if (imagePath == null && edgeDetectionResult == null) {
       return CameraView(
-        controller: controller
+          controller: controller
       );
     }
+    if (live_img != null) {
+      return LiveImagePreview(img:live_img);
+    }
+
+
 
     return ImagePreview(
       imagePath: imagePath,
@@ -79,12 +106,78 @@ class _ScanState extends State<Scan> {
         ResolutionPreset.veryHigh,
         enableAudio: false
     );
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
+    controller.initialize().then((_) async{
+      // Start ImageStream
+      await controller.startImageStream((CameraImage image) => _processCameraImage(image));
+      setState(() {
+        _cameraInitialized = true;
+      });
     });
+
+    // controller.initialize().then((_) {
+    //   if (!mounted) {
+    //     return;
+    //   }
+    //   setState(() {});
+    // });
+  }
+
+  void _processCameraImage(CameraImage image) async {
+    setState(() {
+      _savedImage = image;
+    });
+    imglib.Image img;
+
+    if(Platform.isAndroid){
+      // Allocate memory for the 3 planes of the image
+      Pointer<Uint8> p = allocate(count: _savedImage.planes[0].bytes.length);
+      Pointer<Uint8> p1 = allocate(count: _savedImage.planes[1].bytes.length);
+      Pointer<Uint8> p2 = allocate(count: _savedImage.planes[2].bytes.length);
+
+      // Assign the planes data to the pointers of the image
+      Uint8List pointerList = p.asTypedList(_savedImage.planes[0].bytes.length);
+      Uint8List pointerList1 = p1.asTypedList(_savedImage.planes[1].bytes.length);
+      Uint8List pointerList2 = p2.asTypedList(_savedImage.planes[2].bytes.length);
+      pointerList.setRange(0, _savedImage.planes[0].bytes.length, _savedImage.planes[0].bytes);
+      pointerList1.setRange(0, _savedImage.planes[1].bytes.length, _savedImage.planes[1].bytes);
+      pointerList2.setRange(0, _savedImage.planes[2].bytes.length, _savedImage.planes[2].bytes);
+
+      // Call the convertImage function and convert the YUV to RGB
+      Pointer<Uint32> imgP = conv(p, p1, p2, _savedImage.planes[1].bytesPerRow,
+          _savedImage.planes[1].bytesPerPixel, _savedImage.planes[0].bytesPerRow, _savedImage.height);
+
+      // Get the pointer of the data returned from the function to a List
+      List imgData = imgP.asTypedList((_savedImage.planes[0].bytesPerRow * _savedImage.height));
+      // Generate image from the converted data
+      img = imglib.Image.fromBytes(_savedImage.height, _savedImage.planes[0].bytesPerRow, imgData);
+
+      // Free the memory space allocated
+      // from the planes and the converted data
+      free(p);
+      free(p1);
+      free(p2);
+      free(imgP);
+    }else if(Platform.isIOS){
+      img = imglib.Image.fromBytes(
+        _savedImage.planes[0].bytesPerRow,
+        _savedImage.height,
+        _savedImage.planes[0].bytes,
+        format: imglib.Format.bgra,
+      );
+    }
+
+
+    setState(() {
+      live_img = img;
+    });
+    // Uint8List bytes = img.getBytes();
+    // File file =  File.fromRawPath(bytes);
+    // String filePath = file.path;
+    // print(filePath);
+    //
+    // _detectEdges(filePath);
+
+
   }
 
   @override
@@ -102,7 +195,7 @@ class _ScanState extends State<Scan> {
           onPressed: () {
             if (croppedImagePath == null) {
               return _processImage(
-                imagePath, edgeDetectionResult
+                  imagePath, edgeDetectionResult
               );
             }
 
@@ -117,20 +210,20 @@ class _ScanState extends State<Scan> {
     }
 
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        FloatingActionButton(
-          foregroundColor: Colors.white,
-          child: Icon(Icons.camera_alt),
-          onPressed: onTakePictureButtonPressed,
-        ),
-        SizedBox(width: 16),
-        FloatingActionButton(
-          foregroundColor: Colors.white,
-          child: Icon(Icons.image),
-          onPressed: _onGalleryButtonPressed,
-        ),
-      ]
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            foregroundColor: Colors.white,
+            child: Icon(Icons.camera_alt),
+            onPressed: onTakePictureButtonPressed,
+          ),
+          SizedBox(width: 16),
+          FloatingActionButton(
+            foregroundColor: Colors.white,
+            child: Icon(Icons.image),
+            onPressed: _onGalleryButtonPressed,
+          ),
+        ]
     );
   }
 
@@ -150,6 +243,7 @@ class _ScanState extends State<Scan> {
     if (controller.value.isTakingPicture) {
       return null;
     }
+
 
     try {
       await controller.takePicture(filePath);
@@ -214,11 +308,14 @@ class _ScanState extends State<Scan> {
 
   Padding _getBottomBar() {
     return Padding(
-      padding: EdgeInsets.only(bottom: 32),
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: _getButtonRow()
-      )
+        padding: EdgeInsets.only(bottom: 32),
+        child: Align(
+            alignment: Alignment.bottomCenter,
+            child: _getButtonRow()
+        )
     );
   }
+}
+
+void _startImageStream() {
 }
